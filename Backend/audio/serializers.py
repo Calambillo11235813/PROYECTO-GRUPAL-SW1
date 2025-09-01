4ab17cf5b9bf835bc078eb482274a4b581e7e48e
alexandervillarroel007 <alexandervillarroels700@gmail.com>
2025-08-31 21:35:08 -0400

    espectograma de un adui

--- Backend/audio/serializers.py
+++ Backend/audio/serializers.py
@@ -1,8 +1,9 @@
+# backend/audio/serializers.py
 from rest_framework import serializers
 from .models import AudioUpload
 
 class AudioUploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = AudioUpload
-        fields = ['id', 'file', 'result', 'probability', 'uploaded_at']
-        read_only_fields = ['result', 'probability', 'uploaded_at']
+        fields = ['id', 'file', 'result', 'probability', 'spectrogram', 'uploaded_at']
+        read_only_fields = ['result', 'probability', 'spectrogram', 'uploaded_at']
