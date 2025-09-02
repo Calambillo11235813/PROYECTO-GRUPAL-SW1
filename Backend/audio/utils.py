9918ebb7e76fb09995aff6dba8c07695c0509a1c
imbelfort <belfort.ishmael25@outlook.com>
2025-09-02 08:36:58 -0400

    fixed

--- Backend/audio/utils.py
+++ Backend/audio/utils.py
@@ -1,6 +1,9 @@
 # backend/audio/utils.py
 import librosa
 import librosa.display
+import matplotlib
+# Usar el backend 'Agg' que no requiere GUI
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -12,14 +15,15 @@ def generar_espectrograma(path_audio, filename):
     S_DB = librosa.power_to_db(S, ref=1.0) 
 
 
-    plt.figure(figsize=(12, 2), dpi=120)
-
-    librosa.display.specshow(S_DB, sr=sr, x_axis='time', y_axis='mel', fmax=8000)
-
-
-    plt.colorbar(format='%+2.0f dB')
-    plt.title(f'Espectrograma: {os.path.basename(path_audio)}')
-
+    fig, ax = plt.subplots(figsize=(12, 2), dpi=120)
+    
+    # Configurar el espectrograma
+    img = librosa.display.specshow(S_DB, sr=sr, x_axis='time', y_axis='mel', 
+                                 fmax=8000, ax=ax)
+    
+    # Añadir barra de color
+    plt.colorbar(img, ax=ax, format='%+2.0f dB')
+    ax.set_title(f'Espectrograma: {os.path.basename(path_audio)}')
     plt.tight_layout()
 
     output_dir = os.path.join(settings.MEDIA_ROOT, "plots")
